7c85be32ee9e75531205bfbf2f2e0e1f450eb811
Max Hefley <mizuna737@gmail.com>
2025-05-18 09:22:06 -0600

    Removed leftover freedesktop from awesome conf

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -11,7 +11,6 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
 local lain = require("lain")
-local freedesktop = require("freedesktop")
 local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 local mytable = awful.util.table or gears.table -- 4.{0,1} compatibility
